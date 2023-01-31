be808c2b6bb08c013e39273a673666c9248a03ea
Nian-Rong Wu <105171865+nirowu@users.noreply.github.com>
2023-01-31 15:49:33 +0800

    Time: 21 ms (63.09%), Space: 12.5 MB (5.31%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int>mp;
+        vector<int>ans;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.find(nums[i]) != mp.end()) {
+                ans.push_back(i);
+                ans.push_back(mp[nums[i]]);
+            }
+            mp[target - nums[i]] = i;
+
+        }
+        return ans;
+    }
+};
