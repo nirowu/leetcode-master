16496f9a8554218cf856e353a38dd1f76f568343
Nian-Rong Wu <105171865+nirowu@users.noreply.github.com>
2023-02-02 15:26:16 +0800

    Time: 37 ms (67.48%), Space: 31 MB (21.72%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
-    
+    int cnt(TreeNode* cur) {
+        if (cur == NULL) return 0;
+        int leftnum = cnt(cur->left);
+        int rightnum = cnt(cur->right);
+        
+        return leftnum + rightnum + 1;
+    }
     int countNodes(TreeNode* root) {
-        if (root == NULL) return 0;
-        queue<TreeNode*> que;
-        que.push(root);
-        int ans = 0;
-        while (!que.empty()) {
-            int size = que.size();
-            ans += size;
-            for (int i = 0; i < size; i++) {
-                TreeNode* cur = que.front();
-                que.pop();
-                if(cur->left) que.push(cur->left);
-                if(cur->right) que.push(cur->right);
-            }
-        }
-        return ans;
+        return cnt(root);
     }
 };
