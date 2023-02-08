25cd7d5b36ab5dd260789c8567154e1b2b6340c2
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2023-02-08 09:29:46 +0800

    Time: 108 ms (45.37%), Space: 42.3 MB (43.80%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.cpp
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.cpp
@@ -14,20 +14,19 @@ public:
     unordered_map<int, int> mp;
     TreeNode* dfs(vector<int>&nums, int a, int b) {
         if (a > b) return NULL;
-        int rootVal = nums[a];
+        int rootVal = INT_MIN, pos;
         for (int i = a ; i <= b; i++) {
-            rootVal = max(rootVal, nums[i]);
+            if (rootVal < nums[i]) {
+                rootVal = nums[i];
+                pos = i;
+            }
         }
         TreeNode* root = new TreeNode(rootVal);
-        int pos = mp[rootVal];
         root->left = dfs(nums, a, pos-1);
         root->right = dfs(nums, pos+1, b);
         return root;
     }
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            mp[nums[i]] = i;
-        }
         return dfs(nums, 0, nums.size()-1);
     }
 };
