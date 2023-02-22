3c2e84f5ec5034bb380eabcf0772b28f9f81569f
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2023-02-23 03:05:40 +0800

    Time: 3 ms (93.66%), Space: 11.1 MB (47.97%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    vector<vector<int>> ans;
+    vector<int> path;
+    void backtracking(vector<int>& candidates, int target, int tempSum, int startx) {
+        if (tempSum > target) {
+            return;
+        }
+        if (tempSum == target) {
+            ans.push_back(path);
+            return;
+        }
+        for (int i = startx; i < candidates.size(); i++) {
+            path.push_back(candidates[i]);
+            backtracking(candidates, target, tempSum + candidates[i] , i);
+            path.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        backtracking(candidates, target, 0, 0);
+        return ans;
+    }
+};
