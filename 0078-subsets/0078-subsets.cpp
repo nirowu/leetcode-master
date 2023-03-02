a509708f6165fbfebf906759f7cb1097f8e5c4e8
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2023-03-03 00:16:18 +0800

    Time: 0 ms (100.00%), Space: 7.3 MB (56.72%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    vector<vector<int>> ans;
+    vector<int> tmp;
+    void backtracking(vector<int>& nums, int startx) {
+        ans.push_back(tmp);
+        if (startx >= nums.size()) return;
+        
+        for (int i = startx; i < nums.size(); i++) {
+            tmp.push_back(nums[i]);
+            backtracking(nums, i + 1);
+            tmp.pop_back();
+        }
+        
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        ans.clear(); tmp.clear();
+        backtracking(nums, 0);
+        return ans;
+    }
+};
