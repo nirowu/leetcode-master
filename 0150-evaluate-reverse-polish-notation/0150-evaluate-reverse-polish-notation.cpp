d5d6e36b285e72aa606e03378ab7d445b1e17e9a
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2023-07-10 18:06:10 +0800

    Time: 10 ms (72.68%), Space: 11.8 MB (82.15%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        int ans = 0, num1, num2;
-        stack<int>st;
-        for (int i =  0; i < tokens.size(); i++) {
-            if (tokens[i] == "+") {
-                num1 = st.top(); st.pop();
-                num2 = st.top(); st.pop();
-                ans = num1 + num2;
-                st.push(ans);
+        stack <int> s;
 
-            }
-            else if (tokens[i] == "-"){
-                num2 = st.top(); st.pop();
-                num1 = st.top(); st.pop();
-                ans = num1 - num2;
-                st.push(ans);
+        for (int i = 0; i < tokens.size(); i++) {
+            int fst_num, sec_num;
 
+            if (tokens[i] == "+" ){
+                sec_num = s.top(); s.pop();
+                fst_num = s.top(); s.pop();
+                s.push(fst_num + sec_num);
             }
-            else if (tokens[i] == "*") {
-                num1 = st.top(); st.pop();
-                num2 = st.top(); st.pop();
-                ans = num1 * num2;
-                st.push(ans);
-
+            else if( tokens[i] == "-"){
+                sec_num = s.top(); s.pop();
+                fst_num = s.top(); s.pop();
+                s.push(fst_num - sec_num);
             }
-            else if (tokens[i] == "/") {
-                num2 = st.top(); st.pop();
-                num1 = st.top(); st.pop();
-                ans = num1 / num2;
-                st.push(ans);
+            else if( tokens[i] == "*"){
+                sec_num = s.top(); s.pop();
+                fst_num = s.top(); s.pop();
+                s.push(fst_num * sec_num);
             }
-            else {
-                st.push(stoi(tokens[i]));
+            else if(tokens[i] == "/"){
+                sec_num = s.top(); s.pop();
+                fst_num = s.top(); s.pop();
+                s.push(fst_num / sec_num);
+            }
+            else{
+                int num = stoi(tokens[i]);
+                s.push(num);
             }
         }
-        return st.top();
+        return  s.top();
     }
 };
