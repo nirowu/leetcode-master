975d79ce82d6c694c9f616ebb7513dde09d1c826
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2023-10-27 11:06:06 +0800

    Time: 0 ms (100.00%), Space: 6.6 MB (6.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        int dp[40] = {0, 1};
-        for (int i = 2; i < 40; i++) {
+        int dp[35] = {0, 1};
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
