3038f871193afa69ccefdc75108bdfa70b711cad
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2024-01-02 02:01:49 +0800

    Time: 622 ms (27.13%), Space: 62.1 MB (17.01%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
+    map<pair<int, int>, int> record;
     int dp(int idx, int cur_val, vector<int>&nums, int target) {
+        pair<int, int> pii(idx, cur_val);
+        if (record.find(pii) != record.end()) return record[pii];
         if (idx < 0 && cur_val == target) {
             return 1;
         }
@@ -8,8 +11,11 @@ public:
         
         int neg = dp(idx-1, cur_val-nums[idx], nums, target);
         int pos = dp(idx-1, cur_val+nums[idx], nums, target);
-
-        return neg + pos;
+        
+        int ans = neg + pos;
+        record[pii] = ans;
+        
+        return ans;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         return dp(nums.size()-1, 0, nums, target);
