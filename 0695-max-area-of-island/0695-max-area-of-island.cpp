7eb3c0259b178fdf269d52bf5edadf16b6e18e7b
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2024-07-18 19:53:57 +0800

    Time: 28 ms (9.71%), Space: 52.1 MB (5.02%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,12 +1,12 @@
 
 class Solution {
 public:
-    int bfs(vector<vector<int>>& visited,vector<vector<int>>& grid, int i, int j) {
+    int bfs(vector<vector<int>>& grid, int i, int j) {
 
         int area = 0, x, y;
         int dx[4] = {0, 0, 1, -1};
         int dy[4] = {1, -1, 0, 0};
-        visited[i][j] = -1;
+        grid[i][j] = -1;
         
         queue<pair<int, int>>q;
         q.push({i, j});
@@ -15,9 +15,8 @@ public:
             for (int k = 0; k < 4; k++) {
                 x = pii.first + dx[k];
                 y = pii.second + dy[k];
-                if (x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size() && 
-                    !visited[x][y] && grid[x][y]) {
-                    visited[x][y] = -1;
+                if (x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size()                              && grid[x][y] == 1) {
+                    grid[x][y] = -1;
                     q.push({x, y});
                 }
 
@@ -28,15 +27,12 @@ public:
         return area;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size(), maxArea = 0, curArea = 0;
-        // -1 for visited, 0 for initialization
-        vector<vector<int>>visited(m, vector<int>(n, 0));
+        int m = grid.size(), n = grid[0].size(), maxArea = 0;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] && !visited[i][j]) {
-                    curArea = bfs(visited, grid, i, j);
-                    maxArea = max(maxArea, curArea);
+                if (grid[i][j]) {
+                    maxArea = max(maxArea, bfs(grid, i, j));
                 }
             }
         }
