d0765367c3d7ba961e0ae17f3996b39e3a2e3aa5
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2024-11-17 21:15:34 +0800

    Time: 61 ms (40.30%), Space: 93.6 MB (81.02%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -6,23 +6,15 @@ public:
         int i = 0, j = 1, ans = 0;
         while( j < intervals.size()) {
             if (intervals[i][1] <= intervals[j][0]) {
-                // cout<<intervals[i][1]<<intervals[j][0]<<endl;
                 i = j;
-                // cout<<"1"<<endl;
-            } // 12 34
+            } 
             else if (intervals[i][1] <= intervals[j][1]){
-                // cout<<intervals[i][1]<<intervals[j][0]<<endl;
-
                 ans++;
-                // cout<<"2"<<endl;
-
-            } // 13 2
+            } 
             else if (intervals[i][1] > intervals[j][1]){
                 i = j;
                 ans++;
-                // cout<<"3"<<endl;
-
-            } // 12 12
+            } 
             j++;
         }
         return ans;
