c6c6376eca9a346e64b7a75a43f934929adc482a
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2024-12-19 23:16:56 +0800

    Time: 187 ms (31.79%), Space: 41.9 MB (28.50%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -3,53 +3,34 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int m = nums.size();
         int k = 1;
-        int dp[2505][2505];
-        vector<int>sorted_nums;
-        sorted_nums.assign(nums.begin(), nums.end());
+        int dp[2505][2505] = {0}; 
+        vector<int> sorted_nums(nums);
         sort(sorted_nums.begin(), sorted_nums.end());
-        int removed_dup[2505] = {sorted_nums[0]};
 
         for (int i = 1; i < m; i++) {
             if (sorted_nums[i] != sorted_nums[i-1]) {
-                removed_dup[k++] = sorted_nums[i];
+                sorted_nums[k++] = sorted_nums[i];
             }
         }
-        // // test
-        // for (int i = 0; i < k; i++) {
-        //     cout<<removed_dup[i];
-        // }
-        // cout<<endl;
-        // for (int i = 0; i < m; i++) {
-        //     cout<<nums[i];
-        // }
-        // cout<<endl<<m<<k<<endl;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < k; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = (nums[i] == removed_dup[j])? 1:0; //要延續舊的資訊
-                    continue;
-                }
-                else if (i == 0) {
-                    dp[i][j] = (nums[i] == removed_dup[j])? 1: dp[i][j-1]; 
-                    continue;
-                }
-                else if (j == 0) {
-                    dp[i][j] = (nums[i] == removed_dup[j])? 1: dp[i-1][j]; 
-                    continue;
 
-                }
-                if (nums[i] == removed_dup[j]) {
+        dp[0][0] = (nums[0] == sorted_nums[0]) ? 1 : 0;
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = (nums[i] == sorted_nums[0]) ? 1 : dp[i-1][0];
+        }
+        for (int j = 1; j < k; j++) {
+            dp[0][j] = (nums[0] == sorted_nums[j]) ? 1 : dp[0][j-1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < k; j++) {
+                if (nums[i] == sorted_nums[j]) {
                     dp[i][j] = dp[i-1][j-1] + 1;
-                }
-                else{
+                } else {
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
-
             }
         }
-        
+
         return dp[m-1][k-1];
-        
     }
 };
