542e475cf58c888744732beb593bcbd193ff63b5
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2024-12-20 00:03:01 +0800

    Time: 0 ms (100.00%), Space: 24.5 MB (14.88%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.cpp
+++ 2951-find-the-peaks/2951-find-the-peaks.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> findPeaks(vector<int>& mountain) {
         vector<int> ans;
-        int m = mountain.size() ;
+        int m = mountain.size();
         for (int i = 1; i < m-1; i++) {
             if ((mountain[i] > mountain[i-1]) && (mountain[i] > mountain[i+1])) {
                 ans.push_back(i);
