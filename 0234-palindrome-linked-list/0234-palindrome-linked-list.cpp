8680d73354ac624b448aa026f3e9f4f3f5d74933
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2025-07-16 04:20:09 +0800

    0234-PALINDROME-LINKED-LIST Stats: Time: 0 ms (100%), Space: 118 MB (69.56%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -25,7 +25,6 @@ public:
             prev = slow;
             slow = tmp;
         }
-        cout<<prev->val<<endl;
         ListNode* cur = head;
         while(cur) {
             if (cur->val != prev->val) {
