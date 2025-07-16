0a5cca846bd607c300df72c3177bea2fb246d3b9
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2025-07-16 17:38:39 +0800

    0141-LINKED-LIST-CYCLE Stats: Time: 12 ms (39.46%), Space: 11.8 MB (80.06%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,14 +10,13 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if (!head || !head->next) return false;
-        ListNode* fast = head->next;
+        ListNode* fast = head;
         ListNode* slow = head;
-        while(slow != fast) {
-            if (!fast || !fast->next) return false;
+        while(fast && fast->next) {
             fast = fast->next->next;
             slow = slow->next;
+            if (slow == fast) return true;
         }
-        return true;
-
+        return false;
     }
 };
