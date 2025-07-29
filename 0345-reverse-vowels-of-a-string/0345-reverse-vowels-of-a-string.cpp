1b14d80185bed55f802ceba12400414d596c83fa
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2025-07-29 23:04:49 +0800

    0345-REVERSE-VOWELS-OF-A-STRING Stats: Time: 1 ms (55.09%), Space: 10.3 MB (80.4%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        string tmp = "";
-        for (auto c: s) {
-            char lower_c = tolower(c);
-            if (lower_c == 'a' || lower_c == 'e' || \
-            lower_c == 'i' || lower_c == 'o' || lower_c == 'u') {
-                tmp+= c;
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            char c1 = tolower(s[left]);
+            char c2 = tolower(s[right]);
+
+            if (c1 != 'a' && c1 != 'e' && c1 != 'i' && c1 != 'o' && c1 != 'u') {
+                left++;
+                continue;
             }
-        }
-        int idx = tmp.size()-1;
-        for (auto &c: s) {
-            char lower_c = tolower(c);
-            if (lower_c == 'a' || lower_c == 'e' || \
-            lower_c == 'i' || lower_c == 'o' || lower_c == 'u') {
-                c = tmp[idx--];
+
+            if (c2 != 'a' && c2 != 'e' && c2 != 'i' && c2 != 'o' && c2 != 'u') {
+                right--;
+                continue;
             }
+
+            swap(s[left], s[right]);
+            left++;
+            right--;
         }
+
         return s;
     }
 };
