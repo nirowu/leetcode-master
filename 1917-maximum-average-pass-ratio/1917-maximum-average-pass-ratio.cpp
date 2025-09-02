da6e0866860b3c4bdfa3b12b4136a438a9c0e0b7
Nian-Rong, Wu <105171865+nirowu@users.noreply.github.com>
2025-09-02 16:39:10 +0800

    1917-MAXIMUM-AVERAGE-PASS-RATIO Stats: Time: 263 ms (94.78%), Space: 89.4 MB (97.97%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -1,26 +1,24 @@
 using info = tuple<double, int, int>;
 info A[100005];
-
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
         const int n = classes.size();
-        int idx = 0; double sum = 0.0;
+        double sum = 0.0;
+        int idx = 0;
         for (auto& pq: classes) {
             int p = pq[0], q = pq[1];
-            sum += (double) p/q;
-            double inc = (double)(q - p) / (q * (q + 1.0));
+            double inc = (double)(q-p) / (q * (q + 1.0));
             A[idx++] = {inc, p, q};
+            sum += (double) p/q;
         }
         make_heap(A, A+n);
-
         while(extraStudents--) {
             pop_heap(A, A+n);
             auto [inc, p, q] = A[n-1];
-            if (inc == 0) break; 
-
-            sum += inc; 
-            double newInc = (double)(q - p) / ((q + 1.0) * (q + 2.0));
+            if (inc == 0) break;
+            sum += inc;
+            double newInc = (double)(q-p)/ ((q+1.0) * (q+2.0));
             A[n-1] = {newInc, p+1, q+1};
             push_heap(A, A+n);
         }
